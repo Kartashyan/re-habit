7ff89f17fae67a9a5c42d2ba45104909452094c8
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-30 21:24:41 +0200

    refactor: Add dispatchDomainEvents method to AggregateRoot class

--- src/core/aggregate-root.ts
+++ src/core/aggregate-root.ts
@@ -1,8 +1,23 @@
+import { DomainEvent } from "./domain-event";
 import { Entity } from "./entity";
 import { UID } from "./id";
+import { LocalEventManager } from "./local-event-manager";
 
 export class AggregateRoot<T> extends Entity<T> {
+    protected _domainEvents: DomainEvent<AggregateRoot<T>>[];
     constructor(props: T, id?: UID) {
         super(props, id);
+        this._domainEvents = [];
+    }
+
+    public addDomainEvent(event: DomainEvent<AggregateRoot<T>>): void {
+        this._domainEvents.push(event);
+    }
+
+    dispatchDomainEvents(emitter: LocalEventManager = LocalEventManager.getInstance()): void {
+        this._domainEvents.forEach((event) => {
+            emitter.dispatch(event.name, event);
+        });
+        this._domainEvents = [];
     }
 }
