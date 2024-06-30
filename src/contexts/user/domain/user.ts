8407c595296c3a33e3532bacbab863266f5934a1
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-30 21:24:47 +0200

    feat: Add User class for managing user entities

--- src/contexts/user/domain/user.ts
+++ src/contexts/user/domain/user.ts
@@ -1,1 +1,23 @@
-export {}
+import { AggregateRoot } from "~/core/aggregate-root";
+import { UID } from "~/core/id";
+import { UserCreatedEvent } from "./user-created.event";
+import { Result } from "~/core/result";
+
+interface UserProps {
+    name: string;
+    email: string;
+    password: string;
+}
+export class User extends AggregateRoot<UserProps> {
+    private constructor(props: UserProps, id?: UID) {
+        super(props, id);
+    }
+    public static create(props: UserProps, id?: UID): Result<User> {
+        const isNew = id ? false : true;
+        const user = new User(props, id);
+        if (isNew) {
+            user.addDomainEvent(new UserCreatedEvent(user));
+        }
+        return Result.ok(user);
+    }
+}
