293da2649a731cbefad988f0bb97c9fe7aee47bc
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-06-30 21:59:38 +0200

    feat: Update AggregateRoot class to use generic domain events

--- src/core/aggregate-root.ts
+++ src/core/aggregate-root.ts
@@ -4,14 +4,18 @@ import { UID } from "./id";
 import { LocalEventManager } from "./local-event-manager";
 
 export class AggregateRoot<T> extends Entity<T> {
-    protected _domainEvents: DomainEvent<AggregateRoot<T>>[];
-    constructor(props: T, id?: UID) {
-        super(props, id);
+    protected _domainEvents: DomainEvent<this>[];
+    constructor(_props: T, id?: UID) {
+        super(_props, id);
         this._domainEvents = [];
     }
 
-    public addDomainEvent(event: DomainEvent<AggregateRoot<T>>): void {
+    public addDomainEvent(event: DomainEvent<this>): void {
         this._domainEvents.push(event);
+    }
+
+    get domainEvents(): DomainEvent<AggregateRoot<T>>[] {
+        return this._domainEvents;
     }
 
     dispatchDomainEvents(emitter: LocalEventManager = LocalEventManager.getInstance()): void {
