8b74c7aa1a130c8b6c7e6383aadb181a54a96a7b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-01 19:42:35 +0200

    refactor: Remove unused 'name' property from UserProps interface

--- src/contexts/user/domain/user.ts
+++ src/contexts/user/domain/user.ts
@@ -4,7 +4,6 @@ import { UserCreatedEvent } from "./user-created.event";
 import { Result } from "~/core/result";
 
 interface UserProps {
-    name: string;
     email: string;
     password: string;
 }
@@ -15,6 +14,7 @@ export class User extends AggregateRoot<UserProps> {
     public static create(props: UserProps, id?: UID): Result<User> {
         const isNew = id ? false : true;
         const user = new User(props, id);
+        console.log("----in the user entity----", user);
         if (isNew) {
             user.addDomainEvent(new UserCreatedEvent(user));
         }
