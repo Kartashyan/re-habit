bd6d6d52135def99faac50e5b5d6248e210b352b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-02 21:24:08 +0200

    feat: Add UserService class to inject implementations of user-related operations

---
+++ src/contexts/user/user.service.injection.ts
@@ -0,0 +1,21 @@
+import { SignupUseCase } from "./application/signup.usecase";
+import { UserRepository } from "./domain/user-repo.port";
+import { UserLocalMemoryRepositoryAdapter } from "./infrastructure/persistance/user-repo.adapter";
+
+export class UserService {
+    private readonly userRepo: UserRepository;
+    private readonly signupUseCase: SignupUseCase;
+
+    constructor() {
+        this.userRepo = new UserLocalMemoryRepositoryAdapter();
+        this.signupUseCase = new SignupUseCase(this.userRepo);
+    }
+
+    findUserByEmail(email: string) {
+        return this.userRepo.findByEmail(email);
+    }
+
+    signup(command: { email: string, password: string }) {
+        return this.signupUseCase.execute(command);
+    }
+}
