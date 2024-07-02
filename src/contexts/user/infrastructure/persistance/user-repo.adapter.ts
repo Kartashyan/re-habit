36ba2873ceae502f99f39e1e54f3a48b1dffd7e8
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-02 21:24:34 +0200

    feat: Add UserLocalMemoryRepositoryAdapter for user repository implementation

---
+++ src/contexts/user/infrastructure/persistance/user-repo.adapter.ts
@@ -0,0 +1,18 @@
+import { User } from "../../domain/user";
+import { UserRepository } from "../../domain/user-repo.port";
+
+export class UserLocalMemoryRepositoryAdapter implements UserRepository {
+    private users: User[] = [];
+
+    async save(user: User): Promise<void> {
+        this.users.push(user);
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.users.find((user) => user.email.value === email) || null;
+    }
+
+    async exists(email: string): Promise<boolean> {
+        return !!this.users.find((user) => user.email.value === email);
+    }
+}
