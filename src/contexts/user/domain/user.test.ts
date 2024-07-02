1b75f692c147ea2dd0d7ec647eeb3757945002c4
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-02 18:27:56 +0200

    refactor: Update User class to use Email and Password value objects

--- src/contexts/user/domain/user.test.ts
+++ src/contexts/user/domain/user.test.ts
@@ -1,46 +1,18 @@
 import { describe, expect, it } from "vitest";
 import { User } from "./user";
-import { UserCreatedEvent } from "./user-created.event";
-import { Result } from "~/core/result";
-import { UID } from "~/core/id";
+import { Email } from "./email.value-object";
+import { Password } from "./password.value-object";
 
 describe("User", () => {
-  it("should create a new user with the provided properties", () => {
-    // Arrange
-    const props = {
-      name: "John Doe",
-      email: "john.doe@example.com",
-      password: "password123",
-    };
+    it("should create a new user when valid credentials", () => {
+        // Arrange
+        const email = Email.create("test@example.com");
+        const password = Password.create("password");
 
-    // Act
-    const result = User.create(props);
+        // Act
+        const user = User.create({ email, password });
 
-    // Assert
-    expect(result.isSuccess).toBe(true);
-    expect(result.data).toBeInstanceOf(User);
-    expect(result.data?.props).toEqual(props);
-    expect(result.data?.domainEvents).toHaveLength(1);
-    expect(result.data?.domainEvents[0]).instanceOf(UserCreatedEvent);
-  });
-
-  it("should create a new user with the provided properties and ID", () => {
-    // Arrange
-    const props = {
-      name: "Jane Smith",
-      email: "jane.smith@example.com",
-      password: "password456",
-    };
-    const id = "12345";
-
-    // Act
-    const result = User.create(props, new UID(id));
-
-    // Assert
-    expect(result.isSuccess).toBe(true);
-    expect(result.data).toBeInstanceOf(User);
-    expect(result.data?.props).toEqual(props);
-    expect(result.data?.id.value).toBe(id);
-    expect(result.data?.domainEvents).toEqual([]);
-  });
+        // Assert
+        expect(user).toBeDefined();
+    });
 });
--- src/contexts/user/domain/user.ts
+++ src/contexts/user/domain/user.ts
@@ -1,23 +1,31 @@
 import { AggregateRoot } from "~/core/aggregate-root";
 import { UID } from "~/core/id";
+import { Email } from "./email.value-object";
+import { Password } from "./password.value-object";
 import { UserCreatedEvent } from "./user-created.event";
-import { Result } from "~/core/result";
 
 interface UserProps {
-    email: string;
-    password: string;
+    email: Email;
+    password: Password;
 }
 export class User extends AggregateRoot<UserProps> {
     private constructor(props: UserProps, id?: UID) {
         super(props, id);
     }
-    public static create(props: UserProps, id?: UID): Result<User> {
+    public static create(props: UserProps, id?: UID): User {
         const isNew = id ? false : true;
         const user = new User(props, id);
-        console.log("----in the user entity----", user);
         if (isNew) {
             user.addDomainEvent(new UserCreatedEvent(user));
         }
-        return Result.ok(user);
+        return user;
+    }
+
+    get email(): Email {
+        return this.props.email;
+    }
+
+    get password(): Password {
+        return this.props.password;
     }
 }
