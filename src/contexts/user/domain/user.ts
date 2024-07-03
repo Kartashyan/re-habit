0caac2f09925a32880d08b535d9ca3d395dc1dcf
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-03 23:27:10 +0200

    refactor: Update User class to securely store and compare passwords

--- src/contexts/user/domain/user.ts
+++ src/contexts/user/domain/user.ts
@@ -3,14 +3,18 @@ import { UID } from "~/core/id";
 import { Email } from "./email.value-object";
 import { Password } from "./password.value-object";
 import { UserCreatedEvent } from "./user-created.event";
+import { PasswordService } from "../infrastructure/service/password.service";
 
 interface UserProps {
     email: Email;
     password: Password;
 }
 export class User extends AggregateRoot<UserProps> {
+    public readonly passwordService: PasswordService = new PasswordService();
+    private _hashedPassword: string;
     private constructor(props: UserProps, id?: UID) {
         super(props, id);
+        this._hashedPassword = this.passwordService.hash(props.password);
     }
     public static create(props: UserProps, id?: UID): User {
         const isNew = id ? false : true;
@@ -27,5 +31,9 @@ export class User extends AggregateRoot<UserProps> {
 
     get password(): Password {
         return this.props.password;
+    }
+
+    comparePassword(password: Password): boolean {
+        return this.passwordService.compare(password, this._hashedPassword);
     }
 }
