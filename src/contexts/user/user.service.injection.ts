655103a89bfce08fbeb8fa24bb6bad955e429170
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-12 22:47:24 +0200

    refactor: Update UserService to handle user not found in findUserByEmail method

--- src/contexts/user/user.service.injection.ts
+++ src/contexts/user/user.service.injection.ts
@@ -1,7 +1,9 @@
-import { Result } from "~/core/result";
+import { fail, ok, Result } from "~/core/result";
 import { SignupUseCase } from "./application/signup.usecase";
 import { UserRepository } from "./domain/user-repo.port";
+import { UserModel } from "./domain/user.model";
 import { UserLocalMemoryRepositoryAdapter } from "./infrastructure/persistance/user-repo.adapter";
+import { UserMapper } from "./infrastructure/user.mapper";
 
 export class UserService {
     private readonly userRepo: UserRepository;
@@ -12,8 +14,12 @@ export class UserService {
         this.signupUseCase = new SignupUseCase(this.userRepo);
     }
 
-    findUserByEmail(email: string) {
-        return this.userRepo.findByEmail(email);
+    async findUserByEmail(email: string): Promise<Result<UserModel>> {
+        const user = await this.userRepo.findByEmail(email);
+        if (!user) {
+            return fail("User not found");
+        }
+        return ok(UserMapper.toModel(user));
     }
 
     signup(command: { email: string, password: string }): Promise<Result<true>> {
