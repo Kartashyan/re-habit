2b83c82d854a51fa6137edb9db56a83d3064540c
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-14 20:01:53 +0200

    refactor: Update Signup route with styled form and input components

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -9,15 +9,15 @@ export const action: ActionFunction = signupController;
 export default function Signup() {
     return (
         <section className="flex h-screen flex-col justify-center items-center">
-            <div className="h-72 flex flex-col gap-y-1">
-                <h1 className="text-2xl bg-slate-500">Signup</h1>
+            <div className="h-72 flex flex-col gap-y-1 w-72">
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Signup</h3>
                 <form className="flex flex-col gap-y-1">
                     <Input type="email" id="email" name="email" placeholder="Email" />
                     <Input type="password" id="password" name="password" placeholder="Password" />
                     <Button type="submit">Signup</Button>
                     <p>Already have an account?</p>
                     <p>Go to</p>
-                    <Button asChild>
+                    <Button variant="link" asChild>
                         <Link to="/login">Login</Link>
                     </Button>
                 </form>
