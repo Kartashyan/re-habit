cb6a07effffc000b52f5cc98b7dd3ffe8ceb0f92
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-14 23:53:14 +0200

    refactor: Update Signup route with styled form and input components

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,22 +1,32 @@
 import { ActionFunction } from "@remix-run/node";
+import { Form, Link, useActionData } from "@remix-run/react";
 import { action as signInController } from "~/contexts/user/infrastructure/web/signin.controller";
+import { Button } from "~/shared/ui-lib/button";
+import { Input } from "~/shared/ui-lib/input";
 
 export const action: ActionFunction = signInController;
 
 export default function Login() {
-
+    const actionData = useActionData<typeof action>();
     return (
-        <div className="flex justify-center items-center">
-            <h1 className="text-2xl">Login</h1>
-            <form className="flex flex-col">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" />
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" name="password" />
-                <button type="submit">Login</button>
-
-                <a href="/signup">Sign up</a>
-            </form>
-        </div>
+        <section className="flex h-screen flex-col justify-center items-center">
+            <div className="h-72 flex flex-col gap-y-1 w-72">
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Login</h3>
+                {actionData?.errors ? (
+                    <div className="bg-red-100 text-red-500 p-2 rounded-md">
+                        {actionData.errors.text}
+                    </div>) : null}
+                <Form className="flex flex-col">
+                    <Input type="email" id="email" name="email" placeholder="Email" />
+                    <Input type="password" id="password" name="password" placeholder="Password" />
+                    <Button type="submit">Login</Button>
+                </Form>
+                <p>Don't have an account?</p>
+                <p>Go to</p>
+                <Button variant="link" asChild>
+                    <Link to="/signup">Signup</Link>
+                </Button>
+            </div>
+        </section>
     );
 }
--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,5 +1,5 @@
 import { ActionFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Form, Link, useActionData } from "@remix-run/react";
 import { action as signupController } from "~/contexts/user/infrastructure/web/signup.controller";
 import { Button } from "~/shared/ui-lib/button";
 import { Input } from "~/shared/ui-lib/input";
@@ -7,20 +7,26 @@ import { Input } from "~/shared/ui-lib/input";
 export const action: ActionFunction = signupController;
 
 export default function Signup() {
+    const actionData = useActionData<typeof action>();
+
     return (
         <section className="flex h-screen flex-col justify-center items-center">
             <div className="h-72 flex flex-col gap-y-1 w-72">
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Signup</h3>
-                <form className="flex flex-col gap-y-1">
+                {actionData?.errors ? (
+                    <div className="bg-red-100 text-red-500 p-2 rounded-md">
+                        {actionData.errors.text}
+                    </div>) : null}
+                <Form method="post" className="flex flex-col gap-y-1">
                     <Input type="email" id="email" name="email" placeholder="Email" />
                     <Input type="password" id="password" name="password" placeholder="Password" />
                     <Button type="submit">Signup</Button>
-                    <p>Already have an account?</p>
-                    <p>Go to</p>
-                    <Button variant="link" asChild>
-                        <Link to="/login">Login</Link>
-                    </Button>
-                </form>
+                </Form>
+                <p>Already have an account?</p>
+                <p>Go to</p>
+                <Button variant="link" asChild>
+                    <Link to="/login">Login</Link>
+                </Button>
             </div>
         </section>
     );
