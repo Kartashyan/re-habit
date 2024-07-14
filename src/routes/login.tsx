4a0e160ab9bd4272cea9583f0708a3c9dbb00ebe
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-14 18:16:33 +0200

    refactor: Update Login page with styled form and input components

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,22 @@
+import { ActionFunction } from "@remix-run/node";
+import { action as signInController } from "~/contexts/user/infrastructure/web/signin.controller";
+
+export const action: ActionFunction = signInController;
+
 export default function Login() {
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="flex justify-center items-center">
+            <h1 className="text-2xl">Login</h1>
+            <form className="flex flex-col">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" />
+                <button type="submit">Login</button>
+
+                <a href="/signup">Sign up</a>
+            </form>
         </div>
     );
 }
