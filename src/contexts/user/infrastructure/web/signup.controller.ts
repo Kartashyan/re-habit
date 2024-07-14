f4a3565c4938837ffc3f16b4502460254e8fd380
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-14 23:53:27 +0200

    refactor: Update Signup route with error handling and redirect to login

--- src/contexts/user/infrastructure/web/signup.controller.ts
+++ src/contexts/user/infrastructure/web/signup.controller.ts
@@ -1,18 +1,23 @@
-import { ActionFunction } from "@remix-run/node";
+import { ActionFunction, redirect } from "@remix-run/node";
+import { UserService } from "../../user.service.injection";
 
 export const action: ActionFunction = async ({ request }) => {
     const body = new URLSearchParams(await request.text());
     const email = body.get("email");
     const password = body.get("password");
-
     if (!email || !password) {
-        return new Response("Email and password are required", {
-            status: 400,
-        });
+        return new Response(JSON.stringify({errors: {
+            text: "Email and password are required",
+        
+        }}), {
+            headers: {
+              "Content-Type": "application/json; utf-8",
+            },
+          });
     }
+    const userService = new UserService();
+    await userService.signup({ email, password });
 
-    return new Response("Sign in successful", {
-        status: 200,
-    });
+    return redirect("/login");
 }
 
