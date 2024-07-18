b751e4ff96765cdbc3e5d45ddf1d7ee2759bef13
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-18 23:26:14 +0200

    refactor: Update Password value object to include password hashing

--- src/contexts/user/domain/password.value-object.ts
+++ src/contexts/user/domain/password.value-object.ts
@@ -1,13 +1,17 @@
 import { DomainError } from "~/shared/core/domain.error";
 import { ValueObject } from "~/shared/core/value-object";
+import { PasswordService } from "../infrastructure/service/password.service";
+import { HashedPassword } from "./hashed-password.value-object";
 
 type PasswordValueObjectProps = {
     value: string;
 };
 
 export class Password extends ValueObject<PasswordValueObjectProps> {
+    private passwordService: PasswordService;
     private constructor(props: PasswordValueObjectProps) {
         super(props);
+        this.passwordService = new PasswordService();
     }
 
     public static create(value: string): Password {
@@ -22,6 +26,10 @@ export class Password extends ValueObject<PasswordValueObjectProps> {
 
     public get value(): string {
         return this.props.value;
+    }
+
+    public hash(): HashedPassword {
+        return new HashedPassword(this.passwordService.hash(this));
     }
 
     private static isValid(password: string): boolean {
