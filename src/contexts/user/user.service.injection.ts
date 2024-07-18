b219746fc4e712fcb7a4fa1cd32a332e326f6339
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-18 23:25:29 +0200

    refactor: Update UserService to include signin functionality

--- src/contexts/user/user.service.injection.ts
+++ src/contexts/user/user.service.injection.ts
@@ -1,36 +1,36 @@
 import { fail, ok, Result } from "~/shared/core/result";
+import { SigninUseCase } from "./application/auth/signin.use-case";
 import { SignupUseCase } from "./application/signup.usecase";
 import { UserRepository } from "./domain/user-repo.port";
-import { UserModel } from "./domain/user.model";
+import { User } from "./domain/user.aggregate";
 import { UserLocalMemoryRepositoryAdapter } from "./infrastructure/persistance/user-repo.adapter";
-import { UserMapper } from "./infrastructure/user.mapper";
-import { SigninUseCase } from "./application/auth/signin.use-case";
-import { JwtService } from "./application/auth/jwt.service";
 
 export class UserService {
     private readonly userRepo: UserRepository;
     private readonly signupUseCase: SignupUseCase;
     private readonly signinUseCase: SigninUseCase;
 
-    constructor() {
-        this.userRepo = new UserLocalMemoryRepositoryAdapter();
+    constructor(userRepo: UserRepository) {
+        this.userRepo = userRepo;
         this.signupUseCase = new SignupUseCase(this.userRepo);
-        this.signinUseCase = new SigninUseCase(this.userRepo, new JwtService("secret"));
+        this.signinUseCase = new SigninUseCase(this.userRepo);
     }
 
-    async findUserByEmail(email: string): Promise<Result<UserModel>> {
+    async findUserByEmail(email: string): Promise<Result<User>> {
         const user = await this.userRepo.findByEmail(email);
         if (!user) {
             return fail("User not found");
         }
-        return ok(UserMapper.toModel(user));
+        return ok(user);
     }
 
     signup(command: { email: string, password: string }): Promise<Result<true>> {
         return this.signupUseCase.execute(command);
     }
 
-    signin(command: { email: string, password: string }): Promise<Result<string>> {
+    signin(command: { email: string, password: string }): Promise<Result<User>> {
         return this.signinUseCase.execute(command.email, command.password);
     }
 }
+const userRepo = new UserLocalMemoryRepositoryAdapter();
+export const userService = new UserService(userRepo);
