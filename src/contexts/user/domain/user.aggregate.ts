c4f2f549ea986ae17d629e9aee92226c2fb30e96
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-18 23:26:20 +0200

    refactor: Update User aggregate to use HashedPassword value object for password storage

--- src/contexts/user/domain/user.aggregate.ts
+++ src/contexts/user/domain/user.aggregate.ts
@@ -1,20 +1,17 @@
 import { AggregateRoot } from "~/shared/core/aggregate-root";
 import { UID } from "~/shared/core/id";
 import { Email } from "./email.value-object";
+import { HashedPassword } from "./hashed-password.value-object";
 import { Password } from "./password.value-object";
 import { UserCreatedEvent } from "./user-created.event";
-import { PasswordService } from "../infrastructure/service/password.service";
 
 interface UserProps {
     email: Email;
-    password: Password;
+    hashedPassword: HashedPassword;
 }
 export class User extends AggregateRoot<UserProps> {
-    public readonly passwordService: PasswordService = new PasswordService();
-    private _hashedPassword: string;
     private constructor(props: UserProps, id?: UID) {
         super(props, id);
-        this._hashedPassword = this.passwordService.hash(props.password);
     }
     public static create(props: UserProps, id?: UID): User {
         const isNew = id ? false : true;
@@ -29,11 +26,11 @@ export class User extends AggregateRoot<UserProps> {
         return this.props.email;
     }
 
-    get password(): Password {
-        return this.props.password;
+    get password(): HashedPassword {
+        return this.props.hashedPassword;
     }
 
     comparePassword(password: Password): boolean {
-        return this.passwordService.compare(password, this._hashedPassword);
+        return this.props.hashedPassword.compare(password);
     }
 }
